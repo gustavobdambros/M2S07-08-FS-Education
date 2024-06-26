f486eecfbf66362ec4c9f840e0cd222a99f5de50
gustavobdambros <gustavo_dambros1@hotmail.com>
2024-06-26 20:43:38 -0300

    continuação [M2S07] Ex. 02- Estruturação dos componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { AlunosComponent } from './alunos/alunos.component';
+import { CadastroAlunoComponent } from './cadastro-aluno/cadastro-aluno.component';
+import { DisciplinasComponent } from './disciplinas/disciplinas.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'alunos',
+    component: AlunosComponent,
+  },
+  {
+    path: 'cadastro-aluno',
+    component: CadastroAlunoComponent,
+  },
+  {
+    path: 'disciplinas',
+    component: DisciplinasComponent,
+  },
+];
