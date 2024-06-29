3679e8df0af2fbcf9352f14e13ebabc298768fbe
gustavobdambros <gustavo_dambros1@hotmail.com>
2024-06-29 11:47:54 -0300

    [M2S07] Ex. 04- Criação da tela de cadastro de alunos

--- src/app/cadastro-aluno/cadastro-aluno.component.ts
+++ src/app/cadastro-aluno/cadastro-aluno.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro-aluno',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './cadastro-aluno.component.html',
-  styleUrl: './cadastro-aluno.component.scss'
+  styleUrls: ['./cadastro-aluno.component.scss'],
 })
 export class CadastroAlunoComponent {
+  aluno = {
+    nomeCompleto: '',
+    cpf: '',
+    email: '',
+    celular: '',
+    curso: '',
+  };
 
+  constructor(private router: Router) {}
+
+  salvarAluno() {
+    console.log('Dados do aluno:', this.aluno);
+    alert('Usuário salvo com sucesso!');
+    this.router.navigate(['/alunos']);
+  }
+
+  formValido() {
+    return (
+      this.aluno.nomeCompleto &&
+      this.aluno.cpf &&
+      this.aluno.email &&
+      this.aluno.celular &&
+      this.aluno.curso
+    );
+  }
 }
