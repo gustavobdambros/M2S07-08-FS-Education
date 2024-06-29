3e0509ef591253f9007c7a26f93e9ec931b82989
gustavobdambros <gustavo_dambros1@hotmail.com>
2024-06-29 20:18:15 -0300

    [M2S07] Ex. 07- Criação da tela inicial

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  proximasAtividades = [
+    { titulo: 'Entrega de trabalho da disciplina X', data: '2024-07-05' },
+    { titulo: 'Avaliação da disciplina Y', data: '2024-07-10' },
+    { titulo: 'Chat com o mentor', data: '2024-07-15' },
+  ];
 
+  minhasDisciplinas = ['Matemática', 'Física', 'Programação'];
+
+  cursosExtras = [
+    {
+      titulo: 'Curso de Inglês',
+      descricao: 'Aprimore suas habilidades em inglês.',
+    },
+    {
+      titulo: 'Curso de Fotografia',
+      descricao: 'Aprenda técnicas básicas de fotografia.',
+    },
+  ];
 }
