9b5e9055398dce8164c0e17c39acd1a9cec2aeed
gustavobdambros <gustavo_dambros1@hotmail.com>
2024-06-29 10:05:56 -0300

    [M2S07] Ex. 03- Criação da tela de login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
+
+  onLogin() {
+    if (this.loginForm.valid) {
+      this.router.navigate(['/home']);
+    }
+  }
+
+  onForgotPassword() {
+    alert('Processo de recuperação de senha enviado para o e-mail cadastrado');
+  }
 }
